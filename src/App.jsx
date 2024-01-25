2f48b2e2e7b4e974cb32341f0aed0f33be864c58
Tran Dang Duat <23020023@vnu.edu.vn>
2024-01-25 16:06:48 +0700

    Add Education Section with basic form

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,38 @@ function App() {
       <Leftside 
         fullName = {fullName}
         contactInfos = {contactInfos}
-        changeFullName = {(e) => setFullName(e.target.value)}
-        changePhoneNumber = {(e) => setContactInfos([e.target.value, contactInfos[1], contactInfos[2]])}
-        changeEmail = {(e) => setContactInfos([contactInfos[0], e.target.value, contactInfos[2]])}
-        changeAddress = {(e) => setContactInfos([contactInfos[0], contactInfos[1], e.target.value])}
+        educationalExperiences = {educationalExperiences}
+
+
+        
+        changeFullName = {
+          (e) => setFullName(e.target.value)
+        }
+        changePhoneNumber = {
+          (e) => setContactInfos([e.target.value, contactInfos[1], contactInfos[2]])
+        }
+        changeEmail = {
+          (e) => setContactInfos([contactInfos[0], e.target.value, contactInfos[2]])
+        }
+        changeAddress = {
+          (e) => setContactInfos([contactInfos[0], contactInfos[1], e.target.value])
+        }
+        changeEducation = {
+          (e) => {
+            let index = e.target.parentElement.getAttribute("data-index");
+            // IMPORTANT: 
+            // 'const newEducation = educationalExperiences;' doesn't work here
+            // because then 'newEducation' just references the existing array 
+            const newEducation = [...educationalExperiences];
+            for (let i = 0; i < newEducation.length; i++) {
+              if (newEducation[i].id == index) {
+                newEducation[i][e.target.getAttribute("name")] = e.target.value;
+                break;
+              }
+            }
+            setEducationalExperiences(newEducation);
+          }
+        }
         
       />
       <Rightside 
--- src/components/Leftside.jsx
+++ src/components/Leftside.jsx
@@ -1,27 +1,46 @@
+function Input (props) {
+  return (
+      <input 
+        type = {props.type} 
+        name = {props.name} 
+        id = {props.name}
+        value = {props.value} 
+        onChange = {props.onChange}
+      />
+  );
+}
+
 function GeneralInfo (props) {
   return (
     <form>
-      <label for = "full-name">Full Name:</label>
-      <input 
-        type = "text" name = "full-name" id = "full-name" 
+      <label htmlFor = "full-name">Full Name:</label>
+      <Input 
+        type = "text" 
+        name = "full-name"  
         value = {props.fullName} 
         onChange = {props.changeFullName}
       />
       
-      <label for = "phone-number">Phone number:</label>
-      <input type = "number" name = "phone-number" id = "phone-number" 
+      <label htmlFor = "phone-number">Phone number:</label>
+      <Input 
+        type = "number" 
+        name = "phone-number" 
         value = {props.contactInfos[0]} 
         onChange={props.changePhoneNumber}
       />
       
-      <label for = "email">Email address:</label>
-      <input type = "email" name = "email" id = "email" 
+      <label htmlFor = "email">Email address:</label>
+      <Input 
+        type = "email" 
+        name = "email"
         value = {props.contactInfos[1]}
         onChange = {props.changeEmail}
       />
       
-      <label for = "address">Address:</label>
-      <input type = "text" name = "address" id = "address" 
+      <label htmlFor = "address">Address:</label>
+      <Input 
+        type = "text" 
+        name = "address" 
         value = {props.contactInfos[2]}
         onChange = {props.changeAddress}
       />
@@ -29,7 +48,87 @@ function GeneralInfo (props) {
   );
 }
 
+function Place (props) {
+  return (
+    <form className = "place" data-index = {props.index}>
+      <h1>{props.school}</h1>
+      
+      <label htmlFor = "school">School:</label>
+      <Input 
+        type = "text" 
+        name = "school"
+        value = {props.school}
+        onChange = {props.changeEducation}
+      />
+      
+      <label htmlFor = "from">From:</label>
+      <Input 
+        type = "date" 
+        name = "from"
+        value = {props.from}
+        onChange = {props.changeEducation}
+      />
+      
+      <label htmlFor = "to">To:</label>
+      <Input 
+        type = "date" 
+        name = "to"
+        value = {props.to}
+        onChange = {props.changeEducation}
+      />
+      
+      <label htmlFor = "role">Role:</label>
+      <Input 
+        type = "text" 
+        name = "role"
+        value = {props.role}
+        onChange = {props.changeEducation}
+      />
+    </form>
+  );
+}
+
+function Education (props) {
+  return (
+    <>
+      {props.educationalExperiences.map((place) => {
+        return ( 
+          <Place 
+            from = {place.from}
+            to = {place.to}
+            role = {place.role}
+            school = {place.school}
+            index = {place.id}
+            key = {place.id}
+            changeEducation = {props.changeEducation}
+          />
+        )
+      })}
+    </>
+  );
+}
+
 function Section (props) {
+  let content;
+  if (props.title == "General Information") {
+    content = (
+      <GeneralInfo 
+        fullName = {props.fullName}
+        contactInfos = {props.contactInfos}
+        changeFullName = {props.changeFullName} 
+        changePhoneNumber = {props.changePhoneNumber}
+        changeEmail = {props.changeEmail}
+        changeAddress = {props.changeAddress}
+      />
+    );
+  } else if (props.title == "Education") {
+    content = (
+      <Education
+        educationalExperiences = {props.educationalExperiences}
+        changeEducation = {props.changeEducation}
+      />
+    );   
+  }
   return (
     <section className = "section">
       <div id = "header">
@@ -37,14 +136,7 @@ function Section (props) {
         <h1>{props.title}</h1>
       </div>
       <div id = "content">
-        <GeneralInfo 
-          fullName = {props.fullName}
-          contactInfos = {props.contactInfos}
-          changeFullName = {props.changeFullName} 
-          changePhoneNumber = {props.changePhoneNumber}
-          changeEmail = {props.changeEmail}
-          changeAddress = {props.changeAddress}
-        />
+        {content}
       </div>
     </section>
   );
@@ -62,6 +154,12 @@ function Leftside (props) {
         changePhoneNumber = {props.changePhoneNumber}
         changeEmail = {props.changeEmail}
         changeAddress = {props.changeAddress}
+      />
+      <Section 
+        title = "Education" 
+        key = "2"
+        educationalExperiences = {props.educationalExperiences}
+        changeEducation = {props.changeEducation}
       />
     </div>
   );
